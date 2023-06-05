9fe1bbdc390fea514667c51f9dea180de41d2157
flyangel22 <flyangel1000230@gmail.com>
2023-06-05 15:56:18 +0800

    first commit

---
+++ src/App-old.vue
@@ -0,0 +1,36 @@
+<template>
+  <!----
+    <router-link to="/">首頁</router-link>
+  <router-link to="/guitar">吉他</router-link>
+  ---->
+
+  {{ $route.fullPath }}
+  <input type="button" value="首頁" @click="goto('/')">
+  <input type="button" value="吉他" @click="goto('/guitar')">
+  <input type="button" value="顯示路由" @click="showInfo">
+  <hr>
+  <router-view />
+</template>
+<!----
+  <script>
+export default {
+  methods: {
+    goto(path) {
+      this.$router.push(path)
+    }
+  }
+}
+</script>
+  ---->
+
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+const goto = (path) => {
+  router.push(path)
+}
+const showInfo = () => {
+  console.log(route.fullPath)
+}
+</script>
